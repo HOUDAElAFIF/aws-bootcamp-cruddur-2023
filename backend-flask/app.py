3aecdcd6ee0c7032e309def04fafb788728f39e6
HOUDAELAFIF <ELAFIFE.HOUDA@student.emi.ac.ma>
2023-03-02 21:37:25 +0100

    implimenting rollbar !

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,6 +13,13 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Rollbar --------
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
 # X-Ray --------
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
@@ -70,6 +77,24 @@ xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 
+# rollbar -----------------
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+  """init rollbar module"""
+  rollbar.init(
+    # access token
+    rollbar_access_token,
+    # environment name
+    'production',
+    # server root directory, makes tracebacks prettier
+    root=os.path.dirname(os.path.realpath(__file__)),
+    # flask already sets up logging
+    allow_logging_basic_config=False)
+    # send exceptions from `app` to rollbar, using flask's signal system.
+  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -87,6 +112,10 @@ def after_request(response):
   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
   return response
 
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
