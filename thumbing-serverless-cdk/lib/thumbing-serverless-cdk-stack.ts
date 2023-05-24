bb503206a7a8aadfbaa4838bf93f49bafaf3a698
HOUDAElAFIF <elafife.houda@student.emi.ac.ma>
2023-05-24 16:49:22 +0000

    Implement cdk Stack

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,17 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3 from 'aws-cdk-lib/aws-s3' 
+import * as lambda from 'aws-cdk-lib/aws-lambda' ;
+import * as dotenv from 'dotenv' ;
+
+// load env vars 
+dotenv.config() ;
+
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const BucketName: string = process.env.BUCKET_NAME as string; 
+    const functionPath: string = process.env.FUNCTION_PATH as string; 
+    const folderInput: string = process.env.S3_FOLDER_INPUT as string; 
+    const folderOutput: string = process.env.S3_FOLDER_OUTPUT as string; 
 
-    const bucket = this.create_Bucket(BucketName) ;
+    const bucket = this.createBucket(BucketName) ;
+    const lambda = this.createLambda(functionPath,folderInput,folderOutput,BucketName) ;
   }
 
-  create_Bucket (bucketName:string):s3.IBucket{
+  createBucket (bucketName:string):s3.IBucket{
     const bucket = new s3.Bucket(this,'thumbingBucket',{
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -20,4 +30,24 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return bucket 
     
   }
+
+
+  createLambda(fucntionPath:string,folderIntput:string,folderOutput:string,bucketName:string):lambda.IFunction{
+    const lambdaFuntion = new lambda.Function(this,'lambda',{
+      runtime: lambda.Runtime.NODEJS_18_X ,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(fucntionPath,),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderIntput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+      }
+    
+    }) 
+    return lambdaFuntion ;
+  }
+
+
 }
